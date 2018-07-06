8d1bd42f97919fa6dae82ce188ee98f8a3e59244
½¯־Åô <2210373713@qq.com>
2018-07-06 09:22:48 +0800

    页面详情

---
+++ taotao-portal-web/src/main/java/com/taotao/user/controller/AboutItemController.java
@@ -0,0 +1,7 @@
+package com.taotao.user.controller;
+
+/**
+ * Created by 蒋志鹏 on 2018/7/5.
+ */
+public class AboutItemController {
+}
---
+++ taotao-rest/taotao-rest-interface/src/main/java/com/taotao/rest/service/SearchAboutService.java
@@ -0,0 +1,7 @@
+package com.taotao.rest.service;
+
+/**
+ * Created by 蒋志鹏 on 2018/7/5.
+ */
+public interface SearchAboutService {
+}
